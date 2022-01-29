f7ff6b76e964992a123f96d7a3a64a1532e438d8
sebastian <sebassg8809@gmail.com>
2022-01-29 15:31:31 -0500

    planet2 feature

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,5 +13,9 @@ public class Main {
         Solana.heat="6776767367kj";
         Solana.radius="6473647634km";
         Solana.temp="122131243c";
+
+        Planet2 planet2=new Planet2();
+        planet2.color="yellow";
+        planet2.size="15616516";
     }
 }
